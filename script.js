0d4eb9cb3065568353f152c19e5fa162c8a51f5c
Mustapha Al-Amin Oluwatimmilehin <110484508+timilehin223@users.noreply.github.com>
2025-07-29 19:21:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('scroll', function() {
   const logos = document.getElementById('logos');
   const scrollPosition = window.scrollY;
 
-  const parallaxSpeed = 0.5; // Adjust for desired effect (0 to 1)
+  const parallaxSpeed = 0.5; 
   const offset = scrollPosition * parallaxSpeed;
   logos.style.transform = `translateY(${offset}px)`;
 });
